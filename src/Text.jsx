d6eef2b120052284ed0dd00beae7e87081cc058e
Ahmed <daur.ahmedoo6@gmail.com>
2022-09-07 13:52:12 +0300

    first

---
+++ src/Text.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Text = () => {
+    return (
+        <div>
+            This test file
+            This text was written for react-test-2
+        </div>
+    );
+};
+
+export default Text;
